2a55d578339cef3d93347fcdf280515b751e79fc
Front_Andrew <66951806+99andrew99@users.noreply.github.com>
2023-11-11 13:16:36 +0900

    Title: 두수의 곱과 몫, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 231111/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
+++ 231111/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
@@ -1,4 +1,6 @@
+import math
+
 num1, num2 = map(int, input().split(" "))
 
 print(f"{num1} * {num2} = {num1 * num2}")
-print(f"{num1} / {num2} = {num1 / num2:.0f}")
+print(f"{num1} / {num2} = {math.floor(num1 / num2)}")
